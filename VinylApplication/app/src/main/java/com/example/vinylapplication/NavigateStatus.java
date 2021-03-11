8f3984b35f0bd06ff1519ab6209e56e1f3e4010a
drummduck <nathan.donaldsondrum@gmail.com>
2021-03-11 16:51:38 -0700

    Oops, accidentally did the enum as java...

--- VinylApplication/app/src/main/java/com/example/vinylapplication/NavigateStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.vinylapplication;
-
-public enum NavigateStatus{
-	NONE, SIGN_IN, REGISTER, FAIL_USERNAME, FAIL_PASSWORD, FAIL_NAME
-}
---
+++ VinylApplication/app/src/main/java/com/example/vinylapplication/NavigateStatus.kt
@@ -0,0 +1,5 @@
+package com.example.vinylapplication
+
+enum class NavigateStatus {
+    NONE, SIGN_IN, REGISTER, FAIL_USERNAME, FAIL_PASSWORD, FAIL_NAME
+}
